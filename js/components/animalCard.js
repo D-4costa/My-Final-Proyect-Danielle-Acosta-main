4cf797d6af075715de027d1f2ba737646057f413
D-4costa <danielleelisabeth01@gmail.com>
2026-02-02 18:17:50 -0500

    Update animalCard.js

--- js/components/animalCard.js
+++ js/components/animalCard.js
@@ -1,11 +1,13 @@
 export function animalCard(a){
-  const d=document.createElement('div');
-  d.className='card';
-  d.innerHTML=`
+  const d = document.createElement('div');
+  d.className = 'card';
+  d.innerHTML = `
     <img src="${a.photo}" alt="${a.name}">
     <h3>${a.name}</h3>
     <p>${a.type} • ${a.age}</p>
-    <a href="animal.html?id=${a.id}"><button>View Details</button></a>
+    <a href="./animal.html?id=${a.id}">
+      <button>View Details</button>
+    </a>
   `;
   return d;
 }

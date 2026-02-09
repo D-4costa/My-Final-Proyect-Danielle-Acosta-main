ac3cb759bd27b9ec87508e5d1d4eed5219669c6d
D-4costa <danielleelisabeth01@gmail.com>
2026-02-09 14:23:52 -0500

    Add more animal entries to data.js

--- js/data.js
+++ js/data.js
@@ -1,4 +1,5 @@
 export const animals = [
+  // 🐶 Dogs
   {
     name: "Luna",
     type: "Dog",
@@ -12,6 +13,86 @@ export const animals = [
     image: "https://placedog.net/400/300?id=2"
   },
   {
+    name: "Rocky",
+    type: "Dog",
+    age: "Young",
+    image: "https://placedog.net/400/301?id=3"
+  },
+  {
+    name: "Lucy",
+    type: "Dog",
+    age: "Baby",
+    image: "https://placedog.net/401/300?id=4"
+  },
+  {
+    name: "Buddy",
+    type: "Dog",
+    age: "Senior",
+    image: "https://placedog.net/402/300?id=5"
+  },
+  {
+    name: "Charlie",
+    type: "Dog",
+    age: "Adult",
+    image: "https://placedog.net/403/300?id=6"
+  },
+  {
+    name: "Daisy",
+    type: "Dog",
+    age: "Baby",
+    image: "https://placedog.net/404/300?id=7"
+  },
+  {
+    name: "Toby",
+    type: "Dog",
+    age: "Senior",
+    image: "https://placedog.net/405/300?id=8"
+  },
+  {
+    name: "Cooper",
+    type: "Dog",
+    age: "Young",
+    image: "https://placedog.net/406/300?id=9"
+  },
+  {
+    name: "Molly",
+    type: "Dog",
+    age: "Adult",
+    image: "https://placedog.net/407/300?id=10"
+  },
+  {
+    name: "Bear",
+    type: "Dog",
+    age: "Senior",
+    image: "https://placedog.net/408/300?id=11"
+  },
+  {
+    name: "Bailey",
+    type: "Dog",
+    age: "Baby",
+    image: "https://placedog.net/409/300?id=12"
+  },
+  {
+    name: "Zeus",
+    type: "Dog",
+    age: "Adult",
+    image: "https://placedog.net/410/300?id=13"
+  },
+  {
+    name: "Bruno",
+    type: "Dog",
+    age: "Young",
+    image: "https://placedog.net/411/300?id=14"
+  },
+  {
+    name: "Shadow",
+    type: "Dog",
+    age: "Senior",
+    image: "https://placedog.net/412/300?id=15"
+  },
+
+  // 🐱 Cats
+  {
     name: "Bella",
     type: "Cat",
     age: "Baby",
@@ -24,27 +105,81 @@ export const animals = [
     image: "https://placekitten.com/401/300"
   },
   {
-    name: "Rocky",
-    type: "Dog",
-    age: "Young",
-    image: "https://placedog.net/400/301?id=3"
-  },
-  {
     name: "Milo",
     type: "Cat",
     age: "Young",
     image: "https://placekitten.com/402/300"
   },
   {
-    name: "Lucy",
-    type: "Dog",
-    age: "Baby",
-    image: "https://placedog.net/401/300?id=4"
-  },
-  {
     name: "Chloe",
     type: "Cat",
     age: "Adult",
     image: "https://placekitten.com/403/300"
+  },
+  {
+    name: "Leo",
+    type: "Cat",
+    age: "Senior",
+    image: "https://placekitten.com/404/300"
+  },
+  {
+    name: "Nala",
+    type: "Cat",
+    age: "Baby",
+    image: "https://placekitten.com/405/300"
+  },
+  {
+    name: "Simba",
+    type: "Cat",
+    age: "Young",
+    image: "https://placekitten.com/406/300"
+  },
+  {
+    name: "Loki",
+    type: "Cat",
+    age: "Senior",
+    image: "https://placekitten.com/407/300"
+  },
+  {
+    name: "Cleo",
+    type: "Cat",
+    age: "Adult",
+    image: "https://placekitten.com/408/300"
+  },
+  {
+    name: "Kitty",
+    type: "Cat",
+    age: "Baby",
+    image: "https://placekitten.com/409/300"
+  },
+  {
+    name: "Oscar",
+    type: "Cat",
+    age: "Young",
+    image: "https://placekitten.com/410/300"
+  },
+  {
+    name: "Misty",
+    type: "Cat",
+    age: "Senior",
+    image: "https://placekitten.com/411/300"
+  },
+  {
+    name: "Pepper",
+    type: "Cat",
+    age: "Adult",
+    image: "https://placekitten.com/412/300"
+  },
+  {
+    name: "Snowball",
+    type: "Cat",
+    age: "Baby",
+    image: "https://placekitten.com/413/300"
+  },
+  {
+    name: "Tiger",
+    type: "Cat",
+    age: "Senior",
+    image: "https://placekitten.com/414/300"
   }
 ];

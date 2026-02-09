bca28c8962bd3f7b9aeff3a14a46787a7a0ded9c
D-4costa <danielleelisabeth01@gmail.com>
2026-02-09 15:26:54 -0500

    Update dog image URL for Shadow

--- js/data.js
+++ js/data.js
@@ -88,7 +88,7 @@ export const animals = [
     name: "Shadow",
     type: "Dog",
     age: "Senior",
-    image: "https://placedog.net/400/300?id=15"
+    image: "https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDR8fGRvZ3xlbnwwfHwwfHx8MA%3D%3D"
   },
 
   // 🐱 Cats

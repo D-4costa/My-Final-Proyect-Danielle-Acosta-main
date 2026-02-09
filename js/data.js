51b77b0270b893c665e3fa19c14003a3addd95ad
D-4costa <danielleelisabeth01@gmail.com>
2026-02-09 15:24:24 -0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -4,183 +4,182 @@ export const animals = [
     name: "Luna",
     type: "Dog",
     age: "Young",
-    image: "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?w=400"
+    image: "https://placedog.net/400/300?id=1"
   },
   {
     name: "Max",
     type: "Dog",
     age: "Adult",
-    image: "https://images.unsplash.com/photo-1558788353-f76d92427f16?w=400"
+    image: "https://placedog.net/400/300?id=2"
   },
   {
     name: "Rocky",
     type: "Dog",
     age: "Young",
-    image: "https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?w=400"
+    image: "https://placedog.net/400/300?id=3"
   },
   {
     name: "Lucy",
     type: "Dog",
     age: "Baby",
-    image: "https://images.unsplash.com/photo-1601758125946-6ec2ef64daf8?w=400"
+    image: "https://placedog.net/400/300?id=4"
   },
   {
     name: "Buddy",
     type: "Dog",
     age: "Senior",
-    image: "https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?w=400"
+    image: "https://placedog.net/400/300?id=5"
   },
   {
     name: "Charlie",
     type: "Dog",
     age: "Adult",
-    image: "https://images.unsplash.com/photo-1548199973-03cce0bbc87b?w=400"
+    image: "https://placedog.net/400/300?id=6"
   },
   {
     name: "Daisy",
     type: "Dog",
     age: "Baby",
-    image: "https://images.unsplash.com/photo-1598133894008-61f7fdb8cc3a?w=400"
+    image: "https://placedog.net/400/300?id=7"
   },
   {
     name: "Toby",
     type: "Dog",
     age: "Senior",
-    image: "https://images.unsplash.com/photo-1517849845537-4d257902454a?w=400"
+    image: "https://placedog.net/400/300?id=8"
   },
   {
     name: "Cooper",
     type: "Dog",
     age: "Young",
-    image: "https://images.unsplash.com/photo-1583512603806-077998240c7a?w=400"
+    image: "https://placedog.net/400/300?id=9"
   },
   {
     name: "Molly",
     type: "Dog",
     age: "Adult",
-    image: "https://images.unsplash.com/photo-1534361960057-19889db9621e?w=400"
+    image: "https://placedog.net/400/300?id=10"
   },
   {
     name: "Bear",
     type: "Dog",
     age: "Senior",
-    image: "https://images.unsplash.com/photo-1552053831-71594a27632d?w=400"
+    image: "https://placedog.net/400/300?id=11"
   },
   {
     name: "Bailey",
     type: "Dog",
     age: "Baby",
-    image: "https://images.unsplash.com/photo-1600804340584-c7db2eacf0bf?w=400"
+    image: "https://placedog.net/400/300?id=12"
   },
   {
     name: "Zeus",
     type: "Dog",
     age: "Adult",
-    image: "https://images.unsplash.com/photo-1543466835-00a7907e9de1?w=400"
+    image: "https://placedog.net/400/300?id=13"
   },
   {
     name: "Bruno",
     type: "Dog",
     age: "Young",
-    image: "https://images.unsplash.com/photo-1596492784531-6e6eb5ea9993?w=400"
+    image: "https://placedog.net/400/300?id=14"
   },
-{
-  name: "Shadow",
-  type: "Dog",
-  age: "Senior",
-  image: "https://source.unsplash.com/400x300/?senior-dog"
-},
+  {
+    name: "Shadow",
+    type: "Dog",
+    age: "Senior",
+    image: "https://placedog.net/400/300?id=15"
+  },
 
   // 🐱 Cats
-{
-  name: "Bella",
-  type: "Cat",
-  age: "Baby",
-  image: "https://source.unsplash.com/400x300/?kitten"
-},
+  {
+    name: "Bella",
+    type: "Cat",
+    age: "Baby",
+    image: "https://placekitten.com/400/300"
+  },
   {
     name: "Oliver",
     type: "Cat",
     age: "Adult",
-    image: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?w=400"
+    image: "https://placekitten.com/401/300"
   },
-{
-  name: "Milo",
-  type: "Cat",
-  age: "Young",
-  image: "https://source.unsplash.com/400x300/?young-cat"
-},
+  {
+    name: "Milo",
+    type: "Cat",
+    age: "Young",
+    image: "https://placekitten.com/402/300"
+  },
   {
     name: "Chloe",
     type: "Cat",
     age: "Adult",
-    image: "https://images.unsplash.com/photo-1574158622682-e40e69881006?w=400"
+    image: "https://placekitten.com/403/300"
   },
   {
     name: "Leo",
     type: "Cat",
     age: "Senior",
-    image: "https://images.unsplash.com/photo-1513360371669-4adf3dd7dff8?w=400"
+    image: "https://placekitten.com/404/300"
   },
   {
     name: "Nala",
     type: "Cat",
     age: "Baby",
-    image: "https://images.unsplash.com/photo-1596854407944-bf87f6fdd49e?w=400"
+    image: "https://placekitten.com/405/300"
   },
   {
     name: "Simba",
     type: "Cat",
     age: "Young",
-    image: "https://images.unsplash.com/photo-1543852786-1cf6624b9987?w=400"
+    image: "https://placekitten.com/406/300"
   },
   {
     name: "Loki",
     type: "Cat",
     age: "Senior",
-    image: "https://images.unsplash.com/photo-1495360010541-f48722b34f7d?w=400"
+    image: "https://placekitten.com/407/300"
   },
-{
-  name: "Cleo",
-  type: "Cat",
-  age: "Adult",
-  image: "https://source.unsplash.com/400x300/?adult-cat"
-},
+  {
+    name: "Cleo",
+    type: "Cat",
+    age: "Adult",
+    image: "https://placekitten.com/408/300"
+  },
   {
     name: "Kitty",
     type: "Cat",
     age: "Baby",
-    image: "https://images.unsplash.com/photo-1606214174585-fe31582dc6ee?w=400"
+    image: "https://placekitten.com/409/300"
   },
   {
     name: "Oscar",
     type: "Cat",
     age: "Young",
-    image: "https://images.unsplash.com/photo-1529778873920-4da4926a72c2?w=400"
+    image: "https://placekitten.com/410/300"
   },
-{
-  name: "Misty",
-  type: "Cat",
-  age: "Senior",
-  image: "https://source.unsplash.com/400x300/?senior-cat"
-},
+  {
+    name: "Misty",
+    type: "Cat",
+    age: "Senior",
+    image: "https://placekitten.com/411/300"
+  },
   {
     name: "Pepper",
     type: "Cat",
     age: "Adult",
-    image: "https://images.unsplash.com/photo-1571566882372-1598d88abd90?w=400"
+    image: "https://placekitten.com/412/300"
   },
   {
     name: "Snowball",
     type: "Cat",
     age: "Baby",
-    image: "https://images.unsplash.com/photo-1592194996308-7b43878e84a6?w=400"
+    image: "https://placekitten.com/413/300"
   },
   {
     name: "Tiger",
     type: "Cat",
     age: "Senior",
-    image: "https://images.unsplash.com/photo-1519052537078-e6302a4968d4?w=400"
+    image: "https://placekitten.com/414/300"
   }
 ];
-

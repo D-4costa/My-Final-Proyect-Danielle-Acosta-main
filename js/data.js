16bbb67c285810749443304697a083aa90a66203
D-4costa <danielleelisabeth01@gmail.com>
2026-02-09 15:04:40 -0500

    Update animal images in data.js

--- js/data.js
+++ js/data.js
@@ -4,91 +4,91 @@ export const animals = [
     name: "Luna",
     type: "Dog",
     age: "Young",
-    image: "https://picsum.photos/seed/dog1/400/300"
+    image: "https://images.dog.ceo/breeds/husky/n02110185_1469.jpg"
   },
   {
     name: "Max",
     type: "Dog",
     age: "Adult",
-    image: "https://picsum.photos/seed/dog2/400/300"
+    image: "https://images.dog.ceo/breeds/retriever-golden/n02099601_3004.jpg"
   },
   {
     name: "Rocky",
     type: "Dog",
     age: "Young",
-    image: "https://picsum.photos/seed/dog3/400/300"
+    image: "https://images.dog.ceo/breeds/pug/n02110958_15424.jpg"
   },
   {
     name: "Lucy",
     type: "Dog",
     age: "Baby",
-    image: "https://picsum.photos/seed/dog4/400/300"
+    image: "https://images.dog.ceo/breeds/beagle/n02088364_11136.jpg"
   },
   {
     name: "Buddy",
     type: "Dog",
     age: "Senior",
-    image: "https://picsum.photos/seed/dog5/400/300"
+    image: "https://images.dog.ceo/breeds/germanshepherd/n02106662_5453.jpg"
   },
   {
     name: "Charlie",
     type: "Dog",
     age: "Adult",
-    image: "https://picsum.photos/seed/dog6/400/300"
+    image: "https://images.dog.ceo/breeds/labrador/n02099712_5642.jpg"
   },
   {
     name: "Daisy",
     type: "Dog",
     age: "Baby",
-    image: "https://picsum.photos/seed/dog7/400/300"
+    image: "https://images.dog.ceo/breeds/chihuahua/n02085620_3408.jpg"
   },
   {
     name: "Toby",
     type: "Dog",
     age: "Senior",
-    image: "https://picsum.photos/seed/dog8/400/300"
+    image: "https://images.dog.ceo/breeds/doberman/n02107142_799.jpg"
   },
   {
     name: "Cooper",
     type: "Dog",
     age: "Young",
-    image: "https://picsum.photos/seed/dog9/400/300"
+    image: "https://images.dog.ceo/breeds/boxer/n02108089_1218.jpg"
   },
   {
     name: "Molly",
     type: "Dog",
     age: "Adult",
-    image: "https://picsum.photos/seed/dog10/400/300"
+    image: "https://images.dog.ceo/breeds/pitbull/pitbull_dog.jpg"
   },
   {
     name: "Bear",
     type: "Dog",
     age: "Senior",
-    image: "https://picsum.photos/seed/dog11/400/300"
+    image: "https://images.dog.ceo/breeds/rottweiler/n02106550_10268.jpg"
   },
   {
     name: "Bailey",
     type: "Dog",
     age: "Baby",
-    image: "https://picsum.photos/seed/dog12/400/300"
+    image: "https://images.dog.ceo/breeds/shih-tzu/n02086240_5094.jpg"
   },
   {
     name: "Zeus",
     type: "Dog",
     age: "Adult",
-    image: "https://picsum.photos/seed/dog13/400/300"
+    image: "https://images.dog.ceo/breeds/mastiff-bull/n02108422_4373.jpg"
   },
   {
     name: "Bruno",
     type: "Dog",
     age: "Young",
-    image: "https://picsum.photos/seed/dog14/400/300"
+    image: "https://images.dog.ceo/breeds/bulldog-english/jager-1.jpg"
   },
   {
     name: "Shadow",
     type: "Dog",
     age: "Senior",
-    image: "https://picsum.photos/seed/dog15/400/300"
+    image: "https://images.dog.ceo/breeds/dalmatian/cooper1.jpg"
   },
 
   // 🐱 Cats
@@ -96,91 +96,89 @@ export const animals = [
     name: "Bella",
     type: "Cat",
     age: "Baby",
-    image: "https://placekitten.com/400/300"
+    image: "https://cdn2.thecatapi.com/images/MTY3ODIyMQ.jpg"
   },
   {
     name: "Oliver",
     type: "Cat",
     age: "Adult",
-    image: "https://placekitten.com/401/300"
+    image: "https://cdn2.thecatapi.com/images/MTY3ODIyNA.jpg"
   },
   {
     name: "Milo",
     type: "Cat",
     age: "Young",
-    image: "https://placekitten.com/402/300"
+    image: "https://cdn2.thecatapi.com/images/MTY3ODIyNw.jpg"
   },
   {
     name: "Chloe",
     type: "Cat",
     age: "Adult",
-    image: "https://placekitten.com/403/300"
+    image: "https://cdn2.thecatapi.com/images/MTY3ODIzMA.jpg"
   },
   {
     name: "Leo",
     type: "Cat",
     age: "Senior",
-    image: "https://placekitten.com/404/300"
+    image: "https://cdn2.thecatapi.com/images/MTY3ODIzMw.jpg"
   },
   {
     name: "Nala",
     type: "Cat",
     age: "Baby",
-    image: "https://placekitten.com/405/300"
+    image: "https://cdn2.thecatapi.com/images/MTY3ODIzNg.jpg"
   },
   {
     name: "Simba",
     type: "Cat",
     age: "Young",
-    image: "https://placekitten.com/406/300"
+    image: "https://cdn2.thecatapi.com/images/MTY3ODIzOQ.jpg"
   },
   {
     name: "Loki",
     type: "Cat",
     age: "Senior",
-    image: "https://placekitten.com/407/300"
+    image: "https://cdn2.thecatapi.com/images/MTY3ODI0Mg.jpg"
   },
   {
     name: "Cleo",
     type: "Cat",
     age: "Adult",
-    image: "https://placekitten.com/408/300"
+    image: "https://cdn2.thecatapi.com/images/MTY3ODI0NQ.jpg"
   },
   {
     name: "Kitty",
     type: "Cat",
     age: "Baby",
-    image: "https://placekitten.com/409/300"
+    image: "https://cdn2.thecatapi.com/images/MTY3ODI0OA.jpg"
   },
   {
     name: "Oscar",
     type: "Cat",
     age: "Young",
-    image: "https://placekitten.com/410/300"
+    image: "https://cdn2.thecatapi.com/images/MTY3ODI1MQ.jpg"
   },
   {
     name: "Misty",
     type: "Cat",
     age: "Senior",
-    image: "https://placekitten.com/411/300"
+    image: "https://cdn2.thecatapi.com/images/MTY3ODI1NA.jpg"
   },
   {
     name: "Pepper",
     type: "Cat",
     age: "Adult",
-    image: "https://placekitten.com/412/300"
+    image: "https://cdn2.thecatapi.com/images/MTY3ODI1Nw.jpg"
   },
   {
     name: "Snowball",
     type: "Cat",
     age: "Baby",
-    image: "https://placekitten.com/413/300"
+    image: "https://cdn2.thecatapi.com/images/MTY3ODI2MA.jpg"
   },
   {
     name: "Tiger",
     type: "Cat",
     age: "Senior",
-    image: "https://placekitten.com/414/300"
-  }
-];
+    image: "https://cdn2.thecatapi.com/imag
 

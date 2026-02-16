228d1bd708f23619834df0ecf54c6f856cab35af
D-4costa <danielleelisabeth01@gmail.com>
2026-02-16 18:13:19 -0500

    Create petnames.js

---
+++ js/petnames.js
@@ -0,0 +1,18 @@
+export const dogNames = [
+  "Max", "Luna", "Rocky", "Milo", "Toby",
+  "Bella", "Daisy", "Simba", "Coco", "Zeus",
+  "Nala", "Thor", "Bobby", "Kira", "Bruno",
+  "Rex", "Oreo", "Leo", "Chester", "Lucky"
+];
+
+export const catNames = [
+  "Misty", "Oliver", "Kitty", "Shadow", "Loki",
+  "Simba", "Nina", "Salem", "Pumpkin", "Mochi",
+  "Felix", "Tom", "Garfield", "Kiwi", "Snowball",
+  "Luna", "Milo", "Chloe", "Pepper", "Sushi"
+];
+
+// función util para elegir nombre aleatorio
+export function getRandomName(list) {
+  return list[Math.floor(Math.random() * list.length)];
+}
